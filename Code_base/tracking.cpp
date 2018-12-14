cf95716f3c86de56b2fa271a536381d90326ce16
Himank Goel <himankgoel03@gmail.com>
2018-12-14 17:04:26 +0530

    improved algo

--- Code_base/tracking.cpp
+++ Code_base/tracking.cpp
@@ -8,22 +8,33 @@
 #include <pcl/filters/passthrough.h>
 #include <pcl/filters/voxel_grid.h>
 
-std::string addLeadingZero(int k) {
+struct coordinate {
+    float x, y, z;
+
+    bool operator <(const coordinate& pt) const {
+        return (x < pt.x) || ((!(pt.x < x)) && (y < pt.y)) || ((!(pt.x < x)) && (!(pt.y < y)) && (z < pt.z));
+    }
+};
+
+std::string addLeadingZero(int k)
+{
     std::stringstream ss;
     ss << k;
     std::string final = ss.str();
     int len = final.length();
-    for(int i=len; i<3; i++) {
+    for (int i = len; i < 3; i++)
+    {
         final.insert(0, "0");
     }
     return final;
 }
 
-pcl::PointCloud<pcl::PointXYZ>::Ptr filtering_func(std::string fileName) {
+pcl::PointCloud<pcl::PointXYZ>::Ptr filtering_func(std::string fileName)
+{
 
     pcl::PointCloud<pcl::PointXYZ>::Ptr file(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::PointCloud<pcl::PointXYZ>::Ptr file_filtered(new pcl::PointCloud<pcl::PointXYZ>);
-    
+
     if (pcl::io::loadPCDFile<pcl::PointXYZ>(fileName, *file) == -1) //* load the file
     {
         PCL_ERROR("Couldn't read file test_pcd.pcd \n");
@@ -49,70 +60,137 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr filtering_func(std::string fileName) {
     pass.filter(*file_filtered);
 
     return file_filtered;
-
 }
 
 int main(int argc, char **argv)
 {
-    //To store the human_cloud
-    pcl::PointCloud<pcl::PointXYZ> human_cloud;
-    std::vector<float> human_x_coordinates, human_y_coordinates, human_z_coordinates;
-    
-    // Load Current File for inspection
-    pcl::PointCloud<pcl::PointXYZ>::Ptr current_file_filtered(new pcl::PointCloud<pcl::PointXYZ>);
+    //Variables for maintaining cloud version
+    int current_file_number = 3;
 
-    std::stringstream current_file_name;
-    current_file_name << "../Pcd_Logs/2018-09-26_19-46-31.939/cloud000003" << ".pcd";
-    
-    current_file_filtered = filtering_func(current_file_name.str());
-
-    // Load prev human file
-    pcl::PointCloud<pcl::PointXYZ>::Ptr prev_human_file(new pcl::PointCloud<pcl::PointXYZ>);
-
-    std::stringstream prev_human_file_name;
-    prev_human_file_name << "../build/human_pcd/human_pcd_000002" << ".pcd";
-    
-    if (pcl::io::loadPCDFile<pcl::PointXYZ>(prev_human_file_name.str(), *prev_human_file) == -1) //* load the file
+    for (int l = 4; l <= 50; l++)
     {
-        PCL_ERROR("Couldn't read file human.pcd \n");
-        return (-1);
-    }
+        //To store the human_cloud
+        pcl::PointCloud<pcl::PointXYZ> human_cloud;
+        std::set<coordinate> human_coordinates;
 
-    //Kdtree
-    pcl::KdTreeFLANN<pcl::PointXYZ> kdtree;
-    kdtree.setInputCloud(current_file_filtered);
-        
-    // Aux Variables
-    int h_count =0 ;
+        // Load Current File for inspection
+        pcl::PointCloud<pcl::PointXYZ>::Ptr current_file_filtered(new pcl::PointCloud<pcl::PointXYZ>);
 
-    for(size_t i=0; i<prev_human_file->points.size(); i++) {
-        std::cout<<i<<std::endl;
-        
-        std::vector<int> pointIdxRadiusSearch;
-        std::vector<float> pointRadiusSquaredDistance;
+        std::stringstream current_file_name;
+        current_file_name << "../Pcd_Logs/2018-09-26_19-46-31.939/cloud000" << addLeadingZero(current_file_number) << ".pcd";
 
-        if ( kdtree.radiusSearch (prev_human_file->points[i], 0.03, pointIdxRadiusSearch, pointRadiusSquaredDistance) > 0 ){
-            for (size_t j = 0; j < pointIdxRadiusSearch.size (); ++j) {
-                human_x_coordinates.push_back(current_file_filtered->points[ pointIdxRadiusSearch[j] ].x);
-                human_y_coordinates.push_back(current_file_filtered->points[ pointIdxRadiusSearch[j] ].y);
-                human_z_coordinates.push_back(current_file_filtered->points[ pointIdxRadiusSearch[j] ].z);
-                h_count++;
+        current_file_filtered = filtering_func(current_file_name.str());
+
+        // Load prev human file
+        pcl::PointCloud<pcl::PointXYZ>::Ptr prev_human_file(new pcl::PointCloud<pcl::PointXYZ>);
+
+        std::stringstream prev_human_file_name;
+        prev_human_file_name << "../build/human_pcd/human_pcd_000" << addLeadingZero(current_file_number - 1) << ".pcd";
+
+        if (pcl::io::loadPCDFile<pcl::PointXYZ>(prev_human_file_name.str(), *prev_human_file) == -1) //* load the file
+        {
+            PCL_ERROR("Couldn't read file human.pcd \n");
+            return (-1);
+        }
+
+        //Kdtree
+        pcl::KdTreeFLANN<pcl::PointXYZ> kdtree;
+        kdtree.setInputCloud(current_file_filtered);
+
+        float radius = 0.05;
+        if(prev_human_file->points.size() < 15) {
+            radius = 0.08;
+        }
+
+        // Aux Variables
+        int h_count = 0;
+
+        for (size_t i = 0; i < prev_human_file->points.size(); i++)
+        {
+            std::cout << i << std::endl;
+
+            std::vector<int> pointIdxRadiusSearch;
+            std::vector<float> pointRadiusSquaredDistance;
+
+            if (kdtree.radiusSearch(prev_human_file->points[i], radius, pointIdxRadiusSearch, pointRadiusSquaredDistance) > 0)
+            {
+                for (size_t j = 0; j < pointIdxRadiusSearch.size(); ++j)
+                {
+                    coordinate *current_point = new coordinate;
+                    current_point->x = current_file_filtered->points[pointIdxRadiusSearch[j]].x;
+                    current_point->y = current_file_filtered->points[pointIdxRadiusSearch[j]].y;
+                    current_point->z = current_file_filtered->points[pointIdxRadiusSearch[j]].z;
+                    human_coordinates.insert(*current_point);
+                }
             }
-        }    
+        }
+
+        //To store the bg_cloud
+        pcl::PointCloud<pcl::PointXYZ> back_cloud;
+        std::set<coordinate> back_coordinates;
+        std::set<coordinate> total_coordinates;
+
+        for(size_t m = 0; m<current_file_filtered->points.size(); m++) {
+            coordinate *point = new coordinate;
+            point->x = current_file_filtered->points[m].x;
+            point->y = current_file_filtered->points[m].y;
+            point->z = current_file_filtered->points[m].z;
+            total_coordinates.insert(*point);
+        }
+
+        std::set<coordinate>:: iterator itr;
+        std::set<coordinate>:: iterator it;
+
+        for(itr = total_coordinates.begin(); itr!=total_coordinates.end(); ++itr) {
+            int flag = 0;
+            for(it = human_coordinates.begin(); it!=human_coordinates.end(); it++) {
+                if((*it).x == (*itr).x && (*it).y == (*itr).y && (*it).z == (*itr).z) {
+                    flag = 1;
+                }
+                if(flag != 1) {
+                    back_coordinates.insert(*itr);
+                }
+            }
+        }
+
+        human_cloud.width = human_coordinates.size();
+        human_cloud.height = 1;
+        human_cloud.is_dense = false;
+        human_cloud.points.resize(human_cloud.width * human_cloud.height);
+
+        size_t i = 0;
+        for( it = human_coordinates.begin(); it!=human_coordinates.end(); ++it){
+            human_cloud.points[i].x = (*it).x;
+            human_cloud.points[i].y = (*it).y;
+            human_cloud.points[i].z = (*it).z;
+            ++i;
+        }
+
+        back_cloud.width = back_coordinates.size();
+        back_cloud.height = 1;
+        back_cloud.is_dense = false;
+        back_cloud.points.resize(back_cloud.width * back_cloud.height);
+
+        i = 0;
+        for( it = back_coordinates.begin(); it!=back_coordinates.end(); ++it){
+            back_cloud.points[i].x = (*it).x;
+            back_cloud.points[i].y = (*it).y;
+            back_cloud.points[i].z = (*it).z;
+            ++i;
+        }
+
+        std::stringstream current_human_file_name;
+        current_human_file_name << "../build/human_pcd/human_pcd_000" << addLeadingZero(current_file_number) << ".pcd";
+
+        pcl::io::savePCDFileASCII(current_human_file_name.str(), human_cloud);
+
+        std::stringstream current_back_file_name;
+        current_back_file_name << "../build/back_pcd/back_pcd_000" << addLeadingZero(current_file_number) << ".pcd";
+
+        pcl::io::savePCDFileASCII(current_back_file_name.str(), back_cloud);
+
+
+        current_file_number++;
     }
-
-    human_cloud.width = h_count;
-    human_cloud.height = 1;
-    human_cloud.is_dense = false;
-    human_cloud.points.resize(human_cloud.width * human_cloud.height);
-
-    for (size_t i =0; i<human_cloud.points.size(); ++i) {
-        human_cloud.points[i].x = human_x_coordinates[i];
-        human_cloud.points[i].y = human_y_coordinates[i];
-        human_cloud.points[i].z = human_z_coordinates[i];
-    }
-
-    pcl::io::savePCDFileASCII("../build/human_pcd/human_pcd_000003.pcd", human_cloud);
-
     return (0);
 }
